26e70e4fb881bcb1926edfcc5ccecebd36f79cbf
Lightling <contactlightling@gmail.com>
2022-07-11 21:12:31 -0400

    Update FakeControllerModule.cs
    documentation and cleanup

--- Assets/Goldenwere/GWSU/Controller/Character/InputSystemModule/Samples/SampleProject/Runtime/FakeControllerModule.cs
+++ Assets/Goldenwere/GWSU/Controller/Character/InputSystemModule/Samples/SampleProject/Runtime/FakeControllerModule.cs
@@ -3,26 +3,41 @@ using Goldenwere.GWSU.CTRL.CHAR.Core;
 
 namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule.SampleProject
 {
+    /// <summary>
+    /// Fake module to demonstrate the input module with
+    /// </summary>
     public class FakeControllerModule : BaseControllerModule
     {
         [SerializeField] private BaseInputModule module;
+
+        [Header("Input Emitters")]
         [SerializeField] private ScriptableObject emitterOnCrouch;
         [SerializeField] private ScriptableObject emitterOnJump;
         [SerializeField] private ScriptableObject emitterOnMovement;
 
+        /* actions */
         private BaseModuleAction actionOnCrouch;
         private BaseModuleAction actionOnJump;
         private BaseModuleAction<Vector2> actionOnMovement;
 
+        /* state fields */
         private Vector2 valueMovement;
+        private bool valueCrouched;
 
+        /// <summary>
+        /// Initializes the module on Awake
+        /// </summary>
         private void Awake()
         {
+            // Set up module actions by assigning emitters to the corresponding actions in the module
             actionOnCrouch = new BaseModuleAction(OnCrouch, emitterOnCrouch, ModuleActionType.HeldOrPressed);
             actionOnJump = new BaseModuleAction(OnJump, emitterOnJump, ModuleActionType.Pressed);
             actionOnMovement = new BaseModuleAction<Vector2>(OnMovement, emitterOnMovement, ModuleActionType.Held);
         }
 
+        /// <summary>
+        /// Registers actions on Enable
+        /// </summary>
         private void OnEnable()
         {
             module.RegisterModuleAction(actionOnCrouch);
@@ -30,6 +45,9 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule.SampleProject
             module.RegisterModuleAction(actionOnMovement);
         }
 
+        /// <summary>
+        /// Unregisters actions on Disable
+        /// </summary>
         private void OnDisable()
         {
             module.UnregisterModuleAction(actionOnCrouch);
@@ -37,27 +55,46 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule.SampleProject
             module.UnregisterModuleAction(actionOnMovement);
         }
 
+        /// <summary>
+        /// Handler for the crouch action
+        /// </summary>
         private void OnCrouch()
         {
-            Debug.Log($"Crouched");
+            valueCrouched = !valueCrouched;
         }
 
+        /// <summary>
+        /// Handler for the jump action
+        /// </summary>
         private void OnJump()
         {
             Debug.Log("Jumped");
         }
 
+        /// <summary>
+        /// Handler for the movement action
+        /// </summary>
+        /// <param name="value">The value of the input</param>
         private void OnMovement(Vector2 value)
         {
             valueMovement = value;
         }
 
+        /// <summary>
+        /// Displays testing information to the screen
+        /// </summary>
         private void OnGUI()
         {
+            int size = Screen.height / 32;
             GUI.Label(
-                new Rect(10, 10, Screen.width - 10, 64),
-                $"{valueMovement}",
-                new GUIStyle { fontSize = 64 }
+                new Rect(10, 10, Screen.width - 10, size),
+                $"Movement: {valueMovement}",
+                new GUIStyle { fontSize = size }
+            );
+            GUI.Label(
+                new Rect(10, 10 + size, Screen.width - 10, size),
+                $"Crouched: {valueCrouched}",
+                new GUIStyle { fontSize = size }
             );
         }
     }
