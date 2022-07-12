0e8e07bac5c713b5311f1e945a6ec6c5cc463b3d
Lightling <contactlightling@gmail.com>
2022-07-11 21:04:59 -0400

    Update InputModule.cs
    documentation and cleanup

--- Assets/Goldenwere/GWSU/Controller/Character/InputSystemModule/Runtime/InputModule.cs
+++ Assets/Goldenwere/GWSU/Controller/Character/InputSystemModule/Runtime/InputModule.cs
@@ -6,8 +6,15 @@ using Goldenwere.GWSU.CTRL.CHAR.Core;
 
 namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
 {
+    /// <summary>
+    /// Input Module that implements the UnityEngine's InputSystem.
+    /// Note: The Input Module assumes the InputSystem sets its inputs to default 
+    /// </summary>
     public class InputModule : BaseInputModule
     {
+        /// <summary>
+        /// Container for the actions associated with InputActions
+        /// </summary>
         internal struct Callbacks
         {
             public Action<InputAction.CallbackContext> canceled;
@@ -15,10 +22,13 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
             public Action<InputAction.CallbackContext> started;
         }
 
-        public bool preferToggledInputs;
-
+        // Field backing the RegisteredCallbacks property.
+        // This variable could possibly be null; reference the property instead.
         private Dictionary<Guid, Callbacks> registeredCallbacks;
 
+        /// <summary>
+        /// Callbacks that are currently registered with the Input Module
+        /// </summary>
         private Dictionary<Guid, Callbacks> RegisteredCallbacks
         {
             get
@@ -31,11 +41,16 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
             }
         }
 
+        /// <summary>
+        /// Whether HeldOrPressed inputs prefer Pressed (true) or Held (false)
+        /// </summary>
+        public bool preferToggledInputs;
+
         public override void RegisterModuleAction(BaseModuleAction _action)
         {
             if (TryGetReference(_action.Emitter, out InputActionReference _reference))
             {
-                Action<InputAction.CallbackContext> canceled = (InputAction.CallbackContext ctx) =>
+                void canceled (InputAction.CallbackContext ctx)
                 {
                     switch (_action.Type)
                     {
@@ -51,13 +66,16 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
                     }
                 };
 
-                Action<InputAction.CallbackContext> performed = (InputAction.CallbackContext ctx) =>
+                void performed (InputAction.CallbackContext ctx)
                 {
                     _action.Listener.Invoke();
                 };
 
+                // Subscribe to the corresponding events on the InputAction
                 _reference.action.canceled += canceled;
                 _reference.action.performed += performed;
+
+                // Register the callbacks with the Input Module
                 RegisteredCallbacks.Add
                 (
                     _action.GUID,
@@ -74,7 +92,7 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
         {
             if (TryGetReference(_action.Emitter, out InputActionReference _reference))
             {
-                Action<InputAction.CallbackContext> canceled = (InputAction.CallbackContext ctx) =>
+                void canceled (InputAction.CallbackContext ctx)
                 {
                     switch (_action.Type)
                     {
@@ -89,7 +107,8 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
                             break;
                     }
                 };
-                Action<InputAction.CallbackContext> performed = (InputAction.CallbackContext ctx) =>
+
+                void performed (InputAction.CallbackContext ctx)
                 {
                     switch (_action.Type)
                     {
@@ -98,7 +117,8 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
                             break;
                     }
                 };
-                Action<InputAction.CallbackContext> started = (InputAction.CallbackContext ctx) =>
+
+                void started (InputAction.CallbackContext ctx)
                 {
                     switch (_action.Type)
                     {
@@ -109,9 +129,12 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
                     }
                 };
 
+                // Subscribe to the corresponding events on the InputAction
                 _reference.action.canceled += canceled;
                 _reference.action.performed += performed;
                 _reference.action.started += started;
+
+                // Register the callbacks with the Input Module
                 RegisteredCallbacks.Add
                 (
                     _action.GUID,
@@ -141,9 +164,14 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
             }
         }
 
-        private bool CheckForCallback(Guid id)
+        /// <summary>
+        /// Checks if the callback is registered
+        /// </summary>
+        /// <param name="_id">The id to check for</param>
+        /// <returns>True if registered, false if not</returns>
+        private bool CheckForCallback(Guid _id)
         {
-            if (RegisteredCallbacks.ContainsKey(id))
+            if (RegisteredCallbacks.ContainsKey(_id))
             {
                 return true;
             }
@@ -151,6 +179,12 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
             return false;
         }
 
+        /// <summary>
+        /// Checks if an emitter can be cast to InputActionReference and returns the result
+        /// </summary>
+        /// <param name="_emitter">The emitter to cast to an InputActionReference</param>
+        /// <param name="_reference">The emitter cast to an InputActionReference</param>
+        /// <returns>True if successfully cast, false if not</returns>
         private bool TryGetReference(ScriptableObject _emitter, out InputActionReference _reference)
         {
             if (_emitter is InputActionReference reference)
@@ -166,6 +200,11 @@ namespace Goldenwere.GWSU.CTRL.CHAR.InputSystemModule
             return false;
         }
 
+        /// <summary>
+        /// Generic method to unregister actions from the Input Module
+        /// </summary>
+        /// <param name="_reference">The emitter InputActionReference to unsubscribe from</param>
+        /// <param name="_id">The id of the callbacks to unregister with</param>
         private void Unregister(InputActionReference _reference, Guid _id)
         {
             if (CheckForCallback(_id))
